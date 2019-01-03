8b0f4134e4183e656e3d07dd4acfa532bc31e978
sudheerkomatineni <sudheerkomatineni@gmail.com>
2019-01-02 19:12:01 -0800

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: {
+                src: ["build", 'temp']
+            }
+        },
+        ngtemplates: {
+            common: {
+                src: ['app/**/*.html'],
+                dest: 'temp/all-templates.js',
+                options: {
+                    htmlmin: {
+                        collapseBooleanAttributes: true,
+                        collapseWhitespace: true,
+                        removeAttributeQuotes: true,
+                        removeComments: true, // Only if you don't use comment directives!
+                        removeEmptyAttributes: true,
+                        removeRedundantAttributes: true,
+                        removeScriptTypeAttributes: true,
+                        removeStyleLinkTypeAttributes: true
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: '\n',
+            },
+            jsFiles: {
+                src: [
+                    "bower_components/lodash/lodash.js",
+                    "bower_components/angular/angular.js",
+                    "bower_components/angular-ui-router/release/angular-ui-router.js",
+                    "bower_components/angular-messages/angular-messages.js",
+                    "bower_components/angular-ui-grid/ui-grid.js",
+                    "app/app.js",
+                    "app/**/*.js",
+                    "temp/all-templates.js"
+                ],
+                dest: 'build/<%= pkg.name %>.js',
+            },
+            cssFiles: {
+                src: [
+                    "styleSheets/bootstrap.css",
+                    "bower_components/angular-ui-grid/ui-grid.css",
+                    "styleSheets/style.css"
+                ],
+                dest: 'build/<%= pkg.name %>.css'
+            }
+        },
+        copy: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: ["index.html", "resources/**"],
+                    dest: 'build/'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    src: ['bower_components/angular-ui-grid/*.{ttf,woff,eot,svg}'],
+                    dest: 'build/',
+                    filter: 'isFile'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: ['sass/*.scss', 'app/**/*.html', "index.html"],
+                tasks: ['clean', 'ngtemplates', 'concat', 'copy:build']
+            },
+            js: {
+                files: ['app/**/*.js'],
+                tasks: ['clean', 'ngtemplates', 'concat', 'copy:build']
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'styleSheets'
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-angular-templates');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.registerTask('default', ['clean', 'ngtemplates', 'compass', 'concat', 'copy:build']);
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    mySql = require('mysql'),
+    mySqlConnection = require('./server-db.js'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    bookModule = require("./modules/books/BookService.js"),
+    connection = mySqlConnection.createConnection();
+
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+
+app.use(express.static(__dirname + '\\build'));
+
+app.post('/addBook', function(req, res) {
+    bookModule.addBook(connection, req, res);
+});
+
+app.get('/getBooks', function(req, res) {
+    bookModule.getBooks(connection, req, res);
+});
+
+var server = app.listen(1841, function() {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+});
